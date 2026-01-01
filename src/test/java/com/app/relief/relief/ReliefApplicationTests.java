6e7ea4020eb8cca38e6b298451622f5ee86287f8
Rashad Dweik <rashaddweik20@gmail.com>
2026-01-01 21:12:41 +0200

    .

---
+++ src/test/java/com/app/relief/relief/ReliefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.relief.relief;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReliefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
